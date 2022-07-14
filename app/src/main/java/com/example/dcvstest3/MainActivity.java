6bfb28c97c12b3304f7c525b5e92e80cfe729cee
lee sechang <scleekitec@gmail.com>
2022-07-14 16:49:54 +0900

    add hello world

--- app/src/main/java/com/example/dcvstest3/MainActivity.java
+++ app/src/main/java/com/example/dcvstest3/MainActivity.java
@@ -7,6 +7,7 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
+import android.util.Log;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -26,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.i("TAG", "onCreate: hello world");
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
